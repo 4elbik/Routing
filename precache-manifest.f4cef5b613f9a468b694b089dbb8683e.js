d1c765ad353de9f3f56f09c8173dadf2a3754714
4elbik <4elbik@gmail.com>
2020-05-13 02:30:14 +0300

    Updates

---
+++ precache-manifest.f4cef5b613f9a468b694b089dbb8683e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6a6b2fd69e29434b8697348fdfedb21",
+    "url": "/Routing/index.html"
+  },
+  {
+    "revision": "4b84e5e0526bf8d95b2d",
+    "url": "/Routing/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "40d505c0416af54270f0",
+    "url": "/Routing/static/css/main.6eb98d65.chunk.css"
+  },
+  {
+    "revision": "4b84e5e0526bf8d95b2d",
+    "url": "/Routing/static/js/2.a1b8df5e.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/Routing/static/js/2.a1b8df5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40d505c0416af54270f0",
+    "url": "/Routing/static/js/main.a16f366f.chunk.js"
+  },
+  {
+    "revision": "cd4e18dd7c77019b9ea1",
+    "url": "/Routing/static/js/runtime-main.b243efcc.js"
+  }
+]);
